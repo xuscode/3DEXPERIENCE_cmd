e32f4e2b113a919c902f4aaa7b1ed2beccee3092
song_hsu <song_hsu@163.com>
2022-09-26 17:15:11 +0800

    添加了总体命令行

---
+++ 3DEXPERIENCE_Cmd/CmdList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public enum CmdList
+{
+    Animation,
+    cmd2,
+    cmd3,
+    cmd4,
+    cmd5
+}
+
+
+//namespace _3DEXPERIENCE_Cmd
+//{
+//    internal class CmdList
+//    {
+//    }
+//}
---
+++ 3DEXPERIENCE_Cmd/Program.cs
@@ -0,0 +1,83 @@
+﻿using System.Text;
+
+namespace Sharprompt.Example;
+
+
+
+// ReSharper disable LocalizableElement
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+
+        while (true)
+        {
+            var type = Prompt.Select<CmdList>("Choose prompt example");
+
+            switch (type)
+            {
+                case CmdList.Animation:
+
+                    GeneratateAnimation.GeneratateAnimation.CATMain();
+
+
+                    //RunInputSample();
+
+                    break;
+                case CmdList.cmd2:
+
+                   
+
+                    break;
+                case CmdList.cmd3:
+                    RunPasswordSample();
+                    break;
+                case CmdList.cmd4:
+                    RunSelectSample();
+                    break;
+              
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+
+    private static void RunInputSample()
+    {
+        var name = Prompt.Input<string>("What's your name?", defaultValue: "John Smith", placeholder: "At least 3 characters", validators: new[] { Validators.Required(), Validators.MinLength(3) });
+        Console.WriteLine($"Hello, {name}!");
+    }
+
+    private static void RunConfirmSample()
+    {
+        var answer = Prompt.Confirm("Are you ready?");
+        Console.WriteLine($"Your answer is {answer}");
+    }
+
+    private static void RunPasswordSample()
+    {
+        var secret = Prompt.Password("Type new password", placeholder: "At least 8 characters", validators: new[] { Validators.Required(), Validators.MinLength(8) });
+        Console.WriteLine($"Password OK, {secret}");
+    }
+
+    private static void RunSelectSample()
+    {
+        var city = Prompt.Select("Select your city", new[] { "Seattle", "London", "Tokyo", "New York", "Singapore", "Shanghai" }, pageSize: 3);
+        Console.WriteLine($"Hello, {city}!");
+    }
+
+    private static void RunMultiSelectSample()
+    {
+        var options = Prompt.MultiSelect("Which cities would you like to visit?", new[] { "Seattle", "London", "Tokyo", "New York", "Singapore", "Shanghai" }, pageSize: 3, defaultValues: new[] { "Tokyo" });
+        Console.WriteLine($"You picked {string.Join(", ", options)}");
+    }
+
+
+    private static void RunListSample()
+    {
+        var value = Prompt.List<string>("Please add item(s)");
+        Console.WriteLine($"You picked {string.Join(", ", value)}");
+    }
+
+}
